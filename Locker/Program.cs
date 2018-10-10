2f78504efcf3527efeb25841fb163dda0449ffeb
Miguel Rivas <migueladanrm@outlook.com>
2018-10-09 22:55:13 -0600

    Preparación inicial del proyecto

---
+++ Locker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Locker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
